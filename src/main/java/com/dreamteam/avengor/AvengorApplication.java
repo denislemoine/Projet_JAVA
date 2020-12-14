1f86c01c70b2bac27b0c20f55d1edefad9e84df9
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-12-14 18:18:31 +0100

    config spring + tymeleaf + setup pom.xml check

--- src/main/java/com/dreamteam/avengor/AvengorApplication.java
+++ src/main/java/com/dreamteam/avengor/AvengorApplication.java
@@ -1,33 +1,13 @@
 package com.dreamteam.avengor;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AvengorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvengorApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("DENIS EST UNE SALOPE");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
 	}
 
 }
--- src/test/java/com/dreamteam/avengor/AvengorApplicationTests.java
+++ src/test/java/com/dreamteam/avengor/AvengorApplicationTests.java
@@ -1,12 +1,12 @@
 package com.dreamteam.avengor;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AvengorApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 

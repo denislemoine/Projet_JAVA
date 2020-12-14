702239fc7867304dbb4af5c04938fe3f419595ca
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-12-14 16:14:08 +0100

    add maven + spring dependencies

--- src/main/java/com/dreamteam/avengor/AvengorApplication.java
+++ src/main/java/com/dreamteam/avengor/AvengorApplication.java
@@ -19,7 +19,7 @@ public class AvengorApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			System.out.println("DENIS EST UNE SALOPE");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);

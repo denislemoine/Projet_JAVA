12e657b48fda35f58c8c55efce43cec96f8bbb2d
Denis <59865402+denislemoine@users.noreply.github.com>
2020-12-14 19:10:40 +0100

    Add all controller

--- src/main/java/com/dreamteam/avengor/controller/LoginController.java
+++ src/main/java/com/dreamteam/avengor/controller/LoginController.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Controller
-public class HelloController {
+public class LoginController {
 
     // inject via application.properties
     @Value("${welcome.message}")
---
+++ src/main/java/com/dreamteam/avengor/controller/SuperHerosController.java
@@ -0,0 +1,18 @@
+package com.dreamteam.avengor.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class SuperHerosController {
+
+
+
+}

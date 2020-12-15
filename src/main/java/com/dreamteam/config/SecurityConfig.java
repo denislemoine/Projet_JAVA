5f109025e0442052ad56db42605ea817334a008e
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-12-15 10:02:47 +0100

    add logging system

--- src/main/java/com/dreamteam/config/SecurityConfig.java
+++ src/main/java/com/dreamteam/config/SecurityConfig.java
@@ -1,18 +1,56 @@
 package com.dreamteam.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    private static final String LOGIN_PROCESSING_URL = "/login.html";
+    private static final String LOGIN_FAILURE_URL = "/login?error";
+    private static final String LOGIN_URL = "/login.html";
+    private static final String LOGOUT_SUCCESS_URL = "/login.html";
+
+    @Override
+    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("user2").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                    .authorizeRequests()
+                        .antMatchers("/home*").hasRole("USER")
+                        .antMatchers("/resources/**").permitAll()
+                        .and()
+
+                //LOGIN
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+
+                //LOGOUT
+                .and()
+                .logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+
+    }
+
 }

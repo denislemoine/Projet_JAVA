1f903b67160c60f5a9f43ff5618fbebd20dfeac1
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-12-16 10:07:32 +0100

    Add new Routes + View admin

--- src/main/java/com/dreamteam/avengor/controller/AdminController.java
+++ src/main/java/com/dreamteam/avengor/controller/AdminController.java
@@ -8,10 +8,22 @@ public class AdminController {
     public String adminHome(){
         String login = "admin"; // a changer par la variable contenant le role de l'utilisateur
         if (login == "admin"){
-            return "admin";
+            return "panelAdmin/admin";
         } else {
             return "login";
         }
 
+    }
+    public String adminMission(){
+        return "panelAdmin/missions";
+    }
+    public String adminAccounts(){
+        return "panelAdmin/accounts";
+    }
+    public String adminCrise(){
+        return "panelAdmin/crise";
+    }
+    public String adminOrganisation(){
+        return "panelAdmin/organisations";
     }
 }

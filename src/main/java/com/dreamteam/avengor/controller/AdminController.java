322d84055c6b73c56d78955b3c76637ccf56bbf6
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-12-16 11:09:56 +0100

    add routes

--- src/main/java/com/dreamteam/avengor/controller/AdminController.java
+++ src/main/java/com/dreamteam/avengor/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.dreamteam.avengor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
@@ -17,7 +19,8 @@ public class AdminController {
     public String adminMission(){
         return "panelAdmin/missions";
     }
-    public String adminAccounts(){
+
+    public String adminAccounts(Model model){
         return "panelAdmin/accounts";
     }
     public String adminCrise(){

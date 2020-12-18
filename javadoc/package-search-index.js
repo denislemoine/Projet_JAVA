4cbc72685d80b4ceba0d7dd079fc74432977b6c1
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-12-18 15:59:31 +0100

    upload javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.dreamteam.avengor"},{"l":"com.dreamteam.avengor.controller"},{"l":"com.dreamteam.avengor.database"},{"l":"com.dreamteam.avengor.model"}];updateSearchResults();
